c27f39172b594483a0739dc40c680a3a8ceceda9
ilhamfathoni2 <altakhzin.ham@gmail.com>
2022-01-07 20:03:45 +0700

    Rename middlewares

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,12 +1,8 @@
 const express = require("express");
-
 const { register, login, checkAuth } = require("../controllers/auth");
-
-const { uploadFile } = require("../middleware/uploadFile");
-
+const { uploadFile } = require("../middlewares/uploadFile");
 const router = express.Router();
-
-const { auth } = require("../middleware/auth");
+const { auth } = require("../middlewares/auth");
 
 router.post("/register", register);
 router.post("/login", login);
